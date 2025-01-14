bf2eacfaba3768cfef6b0e4d52737101e51cd3ef
Aubrey Falconer <a@aubreyfalconer.com>
2025-01-13 16:43:29 -0800

    Added a custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { PageWrapper } from "@/components/PageWrapper";
+import { Button, Container, Heading, Text, VStack } from "@chakra-ui/react";
+import Link from "next/link";
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <PageWrapper>
+      <Container maxW="container.xl" py={20}>
+        <VStack gap={8} alignItems="center" textAlign="center">
+          <Heading as="h1" size="4xl">
+            404
+          </Heading>
+          <Heading as="h2" size="xl">
+            Page Not Found
+          </Heading>
+          <Text fontSize="xl" color="gray.500">
+            Oops! The page you're looking for seems to have wandered off into
+            the creative ether.
+          </Text>
+          <Link href="/" passHref>
+            <Button colorScheme="blue" size="lg">
+              Return Home
+            </Button>
+          </Link>
+        </VStack>
+      </Container>
+    </PageWrapper>
+  );
+}
