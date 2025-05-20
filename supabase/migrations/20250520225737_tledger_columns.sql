439822bd90dd8b94d32c08f9a860544da347201c
Graham Jenkins <graham@thegeeky.ninja>
2025-05-20 16:29:12 -0700

    Migrations

---
+++ supabase/migrations/20250520225737_tledger_columns.sql
@@ -0,0 +1,44 @@
+alter table "public"."transaction_ledger" add column "customer_id" text;
+
+alter table "public"."transaction_ledger" add column "payment_intent" text;
+
+alter table "public"."transaction_ledger" add column "payment_method_id" text;
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.calc_budget_raised()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$DECLARE
+  total_amount int;
+  updated_status "PersonStatus";
+BEGIN
+  SELECT SUM(
+    CASE
+      WHEN s.interval = 'year' THEN s.amount / 12
+      ELSE s.amount
+    END
+  ) INTO total_amount
+  FROM subscriptions s
+  WHERE s.beneficiary_id = COALESCE(NEW.beneficiary_id, OLD.beneficiary_id)
+  AND s.status != 'cancelled';
+
+  -- Determine the status based on total amount and budget goal
+  IF total_amount >= (SELECT budget_goal FROM beneficiaries WHERE id = NEW.beneficiary_id)::numeric THEN
+    updated_status = 'Budget Fulfilled';
+  ELSE
+    updated_status = 'Partially Funded';
+  END IF;
+
+  -- Update the beneficiaries record with the new total and status
+  UPDATE beneficiaries
+  SET
+    budget_raised = total_amount,
+    status = updated_status
+  WHERE id = COALESCE(NEW.beneficiary_id, OLD.beneficiary_id);
+
+  RETURN NEW;
+END;$function$
+;
+
+
